a58c2f4f5b2867526310241ea9e7b2cd03431168
Sofi-Y <126502198+Sofi-Y@users.noreply.github.com>
2023-03-01 17:32:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QWidget, QTableView, QApplication
+from PyQt5 import uic  # Импортируем uic
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        # Зададим тип базы данных
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        # Укажем имя базы данных
+        db.setDatabaseName('coffee.db')
+        # И откроем подключение
+        db.open()
+
+        # QTableView - виджет для отображения данных из базы
+        view = QTableView(self)
+        # Создадим объект QSqlTableModel,
+        # зададим таблицу, с которой он будет работать,
+        #  и выберем все данные
+        model = QSqlTableModel(self, db)
+        model.setTable('coffee')
+        model.select()
+
+        # Для отображения данных на виджете
+        # свяжем его и нашу модель данных
+        view.setModel(model)
+        view.move(10, 10)
+        view.resize(617, 315)
+
+        self.setGeometry(300, 100, 650, 450)
+        self.setWindowTitle('Пример работы с QtSql')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
